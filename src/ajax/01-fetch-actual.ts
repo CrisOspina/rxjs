a9581f34dc13894a9a2996dc5064c691f9a7fb62
Cristian Ospina <crisospina523@gmail.com>
2019-12-08 20:46:07 -0500

    [add] update-error

--- src/ajax/01-fetch-actual.ts
+++ src/ajax/01-fetch-actual.ts
@@ -1,19 +1,12 @@
-const url = 'https://api.github.com/users?per_page=5'
+const API = 'https://api.github.com/users?per_page=5'
 
-const manejaErrores = res => {
+const errores = res => {
   if(!res.ok) throw new Error(res.statusText)
   return res
 }
 
-const fetchPromesa = fetch(url)
-
-// fetchPromesa
-//   .then(res => res.json())
-//   .then(console.log)
-//   .catch(err => console.warn('err', err))
-
-fetchPromesa
-  .then(manejaErrores)
+fetch(API)
+  .then(errores)
   .then(res => res.json())
   .then(data => console.log('data: ', data))
-  .catch(err => console.warn('err', err))
+  .catch(err => console.warn('error', err))
